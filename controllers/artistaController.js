92d177c16e8c12456e65e83d1bf7992cc35930aa
Jhoan Sebastian Wilches Jimenez <sebastianwilches2@gmail.com>
2024-06-22 17:19:35 -0500

    Add Endpoint deleteArtista

--- controllers/artistaController.js
+++ controllers/artistaController.js
@@ -6,6 +6,10 @@ const getAllArtistas = async (req = request, res = response) => {
 
     const { rows: result } = await pool.query('SELECT * FROM artista');
 
+    if(result.length === 0){
+        return res.status(404).json({msg:'Artista no encontrado'})
+    }
+
     res.status(200).json({
         result
     })
@@ -16,13 +20,32 @@ const getByIDArtistas = async (req = request, res = response) => {
     const { idArtista } = req.params;
     const { rows: result } = await pool.query("SELECT * FROM artista WHERE idArtista = $1", [idArtista]); //Query dinamica
 
+    if(result.length === 0){
+        return res.status(404).json({msg:'Artista no encontrado'})
+    }
 
     res.status(200).json({
         result
     })
 }
 
+const deleteArtista = async (req = request, res = response) => {
+
+    const { idArtista } = req.params;
+    const { rowCount: result } = await pool.query("DELETE * FROM artista WHERE idArtista = $1", [idArtista]);
+
+    if(rowCount === 0){ //Si es igual a cero significa que no borro ningún registro
+        return res.status(404).json({msg:'Artista no encontrado'})
+    }
+
+    res.status(200).json({
+        result:`Artista con ID ${idArtista} fue eliminado con éxito`
+    })
+
+}
+
 module.exports = {
     getAllArtistas,
-    getByIDArtistas
+    getByIDArtistas,
+    deleteArtista
 }
--- routes/artista.routes.js
+++ routes/artista.routes.js
@@ -1,12 +1,14 @@
 const { Router } = require("express");
-const { getAllArtistas, getByIDArtistas } = require("../controllers/artistaController");
+const { getAllArtistas, getByIDArtistas, deleteArtista } = require("../controllers/artistaController");
 
 const router = Router();
 
 //PETICIONES
 //GET
 router.get('/', getAllArtistas);
-
 router.get('/:idArtista', getByIDArtistas);
+
+//DELETE
+router.delete('/', deleteArtista);
 
 module.exports = router;
