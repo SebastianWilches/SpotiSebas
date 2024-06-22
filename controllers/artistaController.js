d8c5ff239502dd0f83ce996c40d8072c42f225d5
Jhoan Sebastian Wilches Jimenez <sebastianwilches2@gmail.com>
2024-06-22 18:16:58 -0500

    Fix Endpoint updateByIDArtista

--- controllers/artistaController.js
+++ controllers/artistaController.js
@@ -52,14 +52,18 @@ const updateByIDArtista = async (req = request, res = response) => {
     const { nameArtista, pais, dateFormacion, descripcion } = req.body;
 
     //Validar que al actualizar no quede algún campo vacio
-    if(!nameArtista || !pais || !dateFormacion || !descripcion){
-        return res.status(400).json({msg:'Por favor envie todos los campos que componen al Artista'})
+    if (!nameArtista || !pais || !dateFormacion || !descripcion) {
+        return res.status(400).json({ msg: 'Por favor envie todos los campos que componen al Artista' })
     }
 
     const { rows: result } = await pool.query(
         "UPDATE artista SET nameArtista = $1, pais = $2, dateFormacion = $3, descripcion = $4 WHERE idArtista = $5 RETURNING *",
         [nameArtista, pais, dateFormacion, descripcion, idArtista]
-      );
+    );
+
+    if (result.length === 0) { //Si es igual a cero significa que no encontro ningún registro para modificar
+        return res.status(404).json({ msg: 'Artista no encontrado' })
+    }
 
     res.status(201).json({
         result
